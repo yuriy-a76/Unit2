53463ce5ed4052216e8d456d9e2245974f9d6683
yuriy-a76 <yuriyakishin@list.ru>
2018-02-21 23:57:24 +0200

    Module 2

---
+++ app/code/Unit2/Test/Observer/LogPageOutput.php
@@ -0,0 +1,34 @@
+<?php
+namespace Unit2\Test\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+
+class LogPageOutput implements ObserverInterface
+{
+    /**
+     * @var null|\Psr\Log\LoggerInterface
+     */
+    protected $_logger = null;
+    
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+    }
+    
+    /**
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+        $response = $observer->getEvent()->getData('response');
+        $body = $response->getBody();
+        $this->_logger->addDebug("--------\n\n\n BODY \n\n\n ". $body);
+    }
+}
---
+++ app/code/Unit2/Test/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Unit2_Test',
+    __DIR__
+);
